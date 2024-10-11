b1d2825ec24406cac8aa1e2f7e2c1dcccf9979de
Sam Jones <thefoxjones@gmail.com>
2024-10-11 15:05:02 -0500

    retire search

--- pages/_document.js
+++ pages/_document.js
@@ -5,18 +5,7 @@ class MyDocument extends Document {
     return (
       <Html>
         <Head>
-          <link rel="preconnect" href="https://fonts.googleapis.com" />
-          <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
-          <link
-            href="https://fonts.googleapis.com/css2?family=Ubuntu&display=swap"
-            rel="stylesheet"
-          />
-          <link rel="preconnect" href="https://fonts.googleapis.com" />
-          <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
-          <link
-            href="https://fonts.googleapis.com/css2?family=Open+Sans&display=swap"
-            rel="stylesheet"
-          />
+          <title>Rafdo</title>
         </Head>
         <body>
           <Main />
