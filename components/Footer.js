88f49ad972fd8b2427dffcd36a92d0c37b0d6115
Fox <thefoxjones@gmail.com>
2025-09-03 15:39:15 -0500

    move

--- components/Footer.js
+++ components/Footer.js
@@ -7,9 +7,9 @@ function Footer() {
 
       <div className="grid grid-col-1 md:grid-cols-2 lg:grid-cols-3 gap-y-4 grid-flow-row-dense px-8 py-3">
         <div className="flex link justify-center items-center md:col-span-2 lg:col-span-1 lg:col-start-2 font-Ubuntu">{" "}
-          <a href="https://rafdo.vercel.app">
+          <a href="https://rafdo.rf.gd">
             {" "}
-            rafdo.vercel.app{" "}
+            rafdo.rf.gd{" "}
           </a>
         </div>
 
